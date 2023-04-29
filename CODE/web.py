801bc8be6f462b6cb7abbbae39a0408a3d524390
hpleapfrog <88918018+hpleapfrog@users.noreply.github.com>
2023-04-29 15:36:00 +0800

    Update web.py
    fix ipwlan

--- CODE/web.py
+++ CODE/web.py
@@ -18,13 +18,48 @@
 
 #python3
 #_*_ coding:utf-8 _*_
-
+#python3
+#_*_ coding:utf-8 _*_
+import json
+import re
 import urllib.request
 
-UserName = "123456" # 账号
-PassWord = "123456"  # 密码
-Operators = "@cmcc" # 运营商选择：电信“@telecom”，移动“@cmcc”，联通“@unicom”，校园网为空值
 
-api_link = "http://login.hnust.cn:801/eportal/?c=Portal&a=login&callback=dr1004&login_method=1&user_account=,0," + UserName+Operators + "&user_password=" + PassWord +"&wlan_user_ip=10.92.11.123&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=3.3.3"
+def extract_dict(s) -> list:
+    """Extract all valid dicts from a string.
+
+    Args:
+        s (str): A string possibly containing dicts.
+
+    Returns:
+        A list containing all valid dicts.
+
+    """
+    results = []
+    s_ = ' '.join(s.split('\n')).strip()
+    exp = re.compile(r'(\{.*?\})')
+    for i in exp.findall(s_):
+        try:
+            results.append(json.loads(i))
+        except json.JSONDecodeError:
+            pass
+    return results
+
+UserName = "22020402012" # 账号
+PassWord = "148795623"  # 密码
+Operators = "" # 运营商选择：电信“@telecom”，移动“@cmcc”，联通“@unicom”，校园网为空值
+
+get_ip = "http://login.hnust.cn/drcom/chkstatus?callback=dr1002"
+wlanIp = urllib.request.urlopen(get_ip)
+wlanIpStr = wlanIp.read().decode("gbk")
+wlanIpStr = str(wlanIpStr).strip()
+
+wlanIpStr_= extract_dict(wlanIpStr)
+wlan = wlanIpStr_[0]['v46ip']
+
+api_link = "http://login.hnust.cn:801/eportal/?c=Portal&a=login&callback=dr1004&login_method=1&user_account=,0,"+ \
+ UserName + Operators + "&user_password=" + PassWord +"&wlan_user_ip=" + wlan + \
+"&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=3.3.3"
+
 response = urllib.request.urlopen(api_link)
-print(response.read().decode("utf-8"))
+print(response.read().decode("gbk"))
